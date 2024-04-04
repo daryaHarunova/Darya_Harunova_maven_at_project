26797a58059179479f97c4d2d274ae72c329d3d0
Darya Harunova <darya.harunova@gmail.com>
2024-04-04 19:59:01 +0200

    firstCommit

---
+++ src/test/java/FirstUnitTest.java
@@ -0,0 +1,43 @@
+import org.example.UnitClass;
+
+public class FirstUnitTest {
+    public static void main(String[] args) {
+        UnitClass myClass = new UnitClass();
+
+        // First test -> branches -> true true
+
+        int firstActualResult = myClass.getNumber(-1, -100);
+        if (firstActualResult == 149) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+
+        // Second test -> branches -> false false
+
+        int secondActualResult = myClass.getNumber(1, 20);
+        if (secondActualResult == 20) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+
+        // Third test -> branches -> true false
+
+        int thirdActualResult = myClass.getNumber(0, 5);
+        if (thirdActualResult == 50) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+
+        // Fourth test -> branches -> false true
+
+        int fourthActualResult = myClass.getNumber(0, -5);
+        if (fourthActualResult == 30) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+    }
+}
