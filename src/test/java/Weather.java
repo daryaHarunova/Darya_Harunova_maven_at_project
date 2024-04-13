b763086e736c7c41aa268aba9daeaeb17871623f
Darya Harunova <darya.harunova@gmail.com>
2024-04-13 09:09:27 +0200

    added test for weather

--- src/test/java/Weather.java
+++ src/test/java/Weather.java
@@ -3,14 +3,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Weather {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
         driver.findElement(By.id("input")).sendKeys("погода минск weather");
         driver.findElement(By.xpath("//div[@class = 'wM6W7d mus_pc']")).click();
         driver.findElement(By.xpath("//div[@class = 'wob_df'][1]")).click();
         System.out.println();
-
     }
 
 }
